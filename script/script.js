13861c718c171d13bf6438842c0f1972395c75b5
yuseon <yyizi0913@gmail.com>
2023-08-01 18:00:49 +0900

    slide 가변형

--- script/script.js
+++ script/script.js
@@ -45,4 +45,26 @@ const slide2 = new Swiper('#slide2',{
         nextEl: '#slide2 .swiper-button-next',
         prevEl: '#slide2 .swiper-button-prev'
     }
-})
+});
+//pawin-slide2
+const pawin_slide2 = new Swiper('#pawin_slide2',{
+    autoplay:{delay:1000},
+    loop:true,
+    effect:'fade',
+});
+//입양정보 slide
+const dog_info_slide = new Swiper('#dog_info_slide',{
+    slidesPerView:'auto',//breakpoints옵션추가시 'auto', 한번에 보이는 슬라이드 개수
+    spaceBetween:20,//슬라이드 사이 여백
+    //swiper-slide 반응형웹 옵션
+    autoplay:{delay:1000},
+    loop:true,
+    breakpoints:{
+        //해상도:{옵션:값}
+        //해상도 순서는 작은 해상도 -> 큰 해상도 순으로 작성
+        400:{slidesPerView:2}, //400~899
+        // 700:{slidesPerView:2}, //700이상일때
+        900:{slidesPerView:3}, //900이상일때
+        1160:{slidesPerView:4},//1160이상일 경우 슬라이드 4개 표시
+    },
+});
